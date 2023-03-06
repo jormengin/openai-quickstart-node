185318d835a4438c6da91ee7fb02d0a3d277dcb4
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-06 17:54:08 +0100

    wip

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,8 +15,15 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+const animal = req.body.animal || '';
+const petOwner = req.body.petOwner || '';
+const dogType = req.body.dogType || '';
+const theme = req.body.theme || '';
+const tone = req.body.tone || '';
+const length = req.body.length || '';
+let storyType = ''
+
+  if (animal.trim().length === 0 ||petOwner.trim().length === 0 ||dogType.trim().length === 0 ||theme.trim().length === 0 ||tone.trim().length === 0||length.trim().length === 0 ) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
@@ -24,12 +31,29 @@ export default async function (req, res) {
     });
     return;
   }
-
+switch(length){
+  case 'haiku':
+  storyType = 'a haiku'
+  break;
+  case 'poem':
+  storyType = 'a poem'
+  break;
+  case 'short':
+  storyType = 'a short story of about 100 words'
+  break;
+  case 'medium':
+  storyType = 'a medium story of about 250 words'
+  break;
+  case 'long':
+  storyType = 'a long story of about 500 words'
+  break;
+}
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(animal,petOwner,dogType,theme,tone,length),
       temperature: 0.6,
+      max_tokens:500,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -48,15 +72,17 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
+function generatePrompt(animal,petOwner, theme, dogType,tone,storyType) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+    const capitalizedOwner = petOwner[0].toUpperCase() + petOwner.slice(1).toLowerCase();
+  return `Write a ${tone} ${storyType} that meets the following criteria. The story its about a pet and his onwer, and passed the characteristics of the story. Its not needed to add the animal type description. Just dont make a mistake when creating the story. if in dog type you see small its not needed to stay its small if the story doesnt require. But if you dont describe the animal as small, dont say its big. if its white dont say its black. And so forth.
+  If its a poem or a haiku give a poem styling with  breaking paragraphs to emphazise the rhyming parts. this is very important.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  Animal name:${capitalizedAnimal}
+  animal type: ${dogType}
+  animal owner or owners: ${capitalizedOwner}
+  this theme: ${theme}
+`;
+
 }
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,16 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
+  const [petOwnerInput, setPetOwnerInput] = useState("");
+  const [dogTypeInput, setDogTypeInput] = useState("");
+  const [themeInput, setThemeInput] = useState("");
+  const [toneInput, setToneInput] = useState("");
+  const [lengthInput, setLengthInput] = useState("");
   const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
+    setLoading(true);
     event.preventDefault();
     try {
       const response = await fetch("/api/generate", {
@@ -14,44 +21,121 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({
+          animal: animalInput,
+          petOwner: petOwnerInput,
+          dogType: dogTypeInput,
+          theme: themeInput,
+          tone: toneInput,
+          length: lengthInput,
+        }),
       });
 
       const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
 
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   }
+  const handleRefresh = () => {
+    setAnimalInput("");
+    setDogTypeInput("");
+    setLengthInput("");
+    setPetOwnerInput("");
+    setThemeInput("");
+    setToneInput("");
+    setResult((prev) => !prev);
+  };
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Pet Stories</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Create your Pet Story!</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter the name of the protagonist 🐶"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input
+            type="text"
+            name="petOwner"
+            placeholder="Enter a pet owner "
+            value={petOwnerInput}
+            onChange={(e) => setPetOwnerInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="dogType"
+            placeholder="Enter a dog type"
+            value={dogTypeInput}
+            onChange={(e) => setDogTypeInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="theme"
+            placeholder="Enter a theme"
+            value={themeInput}
+            onChange={(e) => setThemeInput(e.target.value)}
+          />
+          <select
+            type="text"
+            name="tone"
+            value={toneInput}
+            onChange={(e) => setToneInput(e.target.value)}
+          >
+            <option value="" disabled>
+              Select the tone
+            </option>
+            <option value="dark">Dark story</option>
+            <option value="light">Light story</option>
+            <option value="funny">Funny story</option>
+            <option value="epic">Epic story</option>
+            <option value="sad">Sad story</option>
+            <option value="happy">Happy story</option>
+          </select>
+          <select
+            type="text"
+            name="length"
+            value={lengthInput}
+            onChange={(e) => setLengthInput(e.target.value)}
+          >
+            <option value="" disabled>
+              Select the type of story
+            </option>
+            <option value="haiku">Haiku</option>
+            <option value="poem">Poem</option>
+            <option value="short">Short story of 100 words</option>
+            <option value="medium">Medium story of 250 words</option>
+            <option value="long">Long story of 500 words</option>
+          </select>
+          <input type="submit" value="Generate story!" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div  onClick={handleRefresh} className={styles.refresh}>
+          Clean form
+        </div>
+        <div className="loader"></div>
+        {loading && <div className={styles.loader}></div>}
+        {!loading && <div className={styles.result}>{result}</div>}
       </main>
     </div>
   );
